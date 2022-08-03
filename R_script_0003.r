2016c90eaa80b039805a2e223e8ba78ffd744ed8
Amir Motefaker <amotef@gmail.com>
2022-08-03 12:32:18 +0430

    Create R_script_0003.r

---
+++ R_script_0003.r
@@ -0,0 +1,23 @@
+# Type Conversion
+# You can convert from one type to another with the following functions:
+  
+# as.numeric()
+# as.integer()
+# as.complex()
+
+x <- 1L # integer
+y <- 2 # numeric
+
+# convert from integer to numeric:
+a <- as.numeric(x)
+
+# convert from numeric to integer:
+b <- as.integer(y)
+
+# print values of x and y
+x
+y
+
+# print the class name of a and b
+class(a)
+class(b)
