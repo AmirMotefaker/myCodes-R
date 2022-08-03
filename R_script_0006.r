4cbc945a532ab53f734e6972b1631e87b6fc95a1
Amir Motefaker <amotef@gmail.com>
2022-08-03 12:45:40 +0430

    Create R_script_0006.r

---
+++ R_script_0006.r
@@ -0,0 +1,12 @@
+# R Booleans / Logical Values
+
+10 > 9    # TRUE because 10 is greater than 9
+10 == 9   # FALSE because 10 is not equal to 9
+10 < 9    # FALSE because 10 is greater than 9
+
+
+a <- 10
+b <- 9
+
+a > b
+# [1] TRUE
