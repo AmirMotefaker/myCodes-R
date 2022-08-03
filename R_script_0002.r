18c3dc0c10711fb053afba7671d251cdb359e086
Amir Motefaker <amotef@gmail.com>
2022-08-03 12:27:45 +0430

    Create R_script_0002.r

---
+++ R_script_0002.r
@@ -0,0 +1,28 @@
+# Basic data types in R can be divided into the following types:
+  
+# numeric - (10.5, 55, 787)
+# integer - (1L, 55L, 100L, where the letter "L" declares this as an integer)
+# complex - (9 + 3i, where "i" is the imaginary part)
+# character (a.k.a. string) - ("k", "R is exciting", "FALSE", "11.5")
+# logical (a.k.a. boolean) - (TRUE or FALSE)
+
+
+# numeric
+x <- 10.5
+class(x)
+
+# integer
+x <- 1000L
+class(x)
+
+# complex
+x <- 9i + 3
+class(x)
+
+# character/string
+x <- "R is exciting"
+class(x)
+
+# logical/boolean
+x <- TRUE
+class(x)
