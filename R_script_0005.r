69fa8e7e4d1319ed51264a46b2e797f7f34e5308
Amir Motefaker <amotef@gmail.com>
2022-08-03 12:41:48 +0430

    Create R_script_0005.r

---
+++ R_script_0005.r
@@ -0,0 +1,58 @@
+# R Strings
+
+"hello"
+'hello'
+
+#[1] "hello"
+#[1] "hello"
+
+str <- "Hello"
+str # print the value of str
+# [1] "Hello"
+
+str <- "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+str # print the value of str
+# [1] "Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit,\nsed do eiusmod tempor incididunt\nut labore et dolore magna aliqua."
+
+str <- "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+cat(str)
+# orem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua.
+
+str <- "Hello World!"
+nchar(str)
+# [1] 12
+
+
+str <- "Hello World!"
+
+grepl("H", str)
+# [1] TRUE
+grepl("Hello", str)
+# [1] TRUE
+grepl("X", str)
+# [1] FALSE
+
+
+str1 <- "Hello"
+str2 <- "World"
+
+paste(str1, str2)
+# [1] "Hello World"
+
+str <- "We are the so-called \"Vikings\", from the north."
+
+str
+# [1] "We are the so-called \"Vikings\", from the north."
+cat(str)
+# We are the so-called "Vikings", from the north.
