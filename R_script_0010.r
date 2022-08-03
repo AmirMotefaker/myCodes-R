25e1d4a877000550900171e31b2d8b1dfced9ed6
Amir Motefaker <amotef@gmail.com>
2022-08-03 13:31:27 +0430

    Create R_script_0010.r

---
+++ R_script_0010.r
@@ -0,0 +1,67 @@
+# R While Loop
+
+
+i <- 1
+while (i < 6) {
+  print(i)
+  i <- i + 1
+}
+
+# [1] 1
+# [1] 2
+# [1] 3
+# [1] 4
+# [1] 5
+
+
+i <- 1
+while (i < 6) {
+  print(i)
+  i <- i + 1
+  if (i == 4) {
+    break
+  }
+}
+
+# [1] 1
+# [1] 2
+# [1] 3
+
+
+
+# Next
+i <- 0
+while (i < 6) {
+  i <- i + 1
+  if (i == 3) {
+    next
+  }
+  print(i)
+}
+
+# [1] 1
+# [1] 2
+# [1] 4
+# [1] 5
+# [1] 6
+
+
+
+# Amir!
+# If .. Else Combined with a While Loop
+dice <- 1
+while (dice <= 6) {
+  if (dice < 6) {
+    print("No Amir")
+  } else {
+    print("Amir!")
+  }
+  dice <- dice + 1
+}
+
+# [1] "No Amir"
+# [1] "No Amir"
+# [1] "No Amir"
+# [1] "No Amir"
+# [1] "No Amir"
+# [1] "Amir!"
