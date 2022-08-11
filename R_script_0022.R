6df40ee8d435683cf2f8624c0809770927026b67
Amir Motefaker <amotef@gmail.com>
2022-08-11 09:20:49 +0430

    Create R_script_0022.R

---
+++ R_script_0022.R
@@ -0,0 +1,37 @@
+# R Scatter Plot
+
+
+
+# Scatter Plots
+# A "scatter plot" is a type of plot used to display the relationship between
+# two numerical variables, and plots one dot for each observation.
+# It needs two vectors of same length, one for the x-axis (horizontal)
+# and one for the y-axis (vertical)
+
+x <- c(5,7,8,7,2,2,9,4,11,12,9,6)
+y <- c(99,86,87,88,111,103,87,94,78,77,85,86)
+
+plot(x, y)
+
+
+
+# add a header and different labels to describe the scatter plot better
+x <- c(5,7,8,7,2,2,9,4,11,12,9,6)
+y <- c(99,86,87,88,111,103,87,94,78,77,85,86)
+
+plot(x, y, main="Observation of Cars", xlab="Car age", ylab="Car speed")
+
+
+
+# Compare Plots
+# To compare the plot with another plot, use the points() function
+# day one, the age and speed of 12 cars:
+x1 <- c(5,7,8,7,2,2,9,4,11,12,9,6)
+y1 <- c(99,86,87,88,111,103,87,94,78,77,85,86)
+
+# day two, the age and speed of 15 cars:
+x2 <- c(2,2,8,1,15,8,12,9,7,3,11,4,7,14,12)
+y2 <- c(100,105,84,105,90,99,90,95,94,100,79,112,91,80,85)
+
+plot(x1, y1, main="Observation of Cars", xlab="Car age", ylab="Car speed", col="red", cex=2)
+points(x2, y2, col="blue", cex=2)
