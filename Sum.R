d445bd9f55e0f6cc2c901a3896a4e9b150cd8fb7
Amir Motefaker <amotef@gmail.com>
2022-08-21 11:46:56 +0430

    Update Sum.R

--- Sum.R
+++ Sum.R
@@ -1,7 +1,10 @@
+# Sums of 2 input numbers
 Num1=readline(prompt="Enter the 1st number:")
 Num1=as.integer(Num1)
 Num2=readline(prompt="Enter the 2st number:")
 Num2=as.integer(Num2)
 Sum=Num1+Num2
 print(Sum)
-# [1] 25
+# Enter the 1st number:8
+# Enter the 2st number:13
+# [1] 21
