0fe10f89a0308b2e70f974d57477b523d5542e11
Amir Motefaker <amotef@gmail.com>
2022-09-06 16:13:21 +0430

    Add files via upload

--- exercise.R
+++ exercise.R
@@ -1970,3 +1970,166 @@ price1
 #      [,1] [,2]
 # [1,]   10   30
 # [2,]   20   40
+
+
+
+#----------Basic data type functions------------#
+n1<-10
+class(n1) # Returns the datatype of the variable
+# [1] "numeric"
+
+typeof(n1)
+# [1] "double"
+
+n2<-10.5
+class(n2)
+# [1] "numeric"
+
+typeof(n2)
+# [1] "double"
+
+
+is.numeric(n1)  #is.numeric() function returns True if given value is numeric else false
+# [1] TRUE
+
+is.numeric(n2)
+# [1] TRUE
+
+
+I1<-as.integer(10) #as.integer converts the given value into integer
+I1
+# [1] 10
+
+I2<-as.integer(10.5)
+I2
+# [1] 10
+
+I3<-as.integer("10")
+class(I2)
+# [1] "integer"
+
+class(I1)
+# [1] "integer"
+
+class(I3)
+# [1] "integer"
+
+
+#All integers are numeric but all numerics are not integers.
+
+is.numeric(I1)
+# [1] TRUE
+
+is.integer(I1)
+# [1] TRUE
+
+
+c1<-"John"
+c2<-"Rob"
+class(c1)
+# [1] "character"
+
+class(c2)
+# [1] "character"
+
+is.character(c1)
+# [1] TRUE
+
+is.character(c2)
+# [1] TRUE
+
+
+toupper(c1) # Converts to uppercase
+# [1] "JOHN"
+
+substr(c1,1,2) # Extract set of characters
+# [1] "Jo"
+
+name<-paste(c1,c2)
+name
+# [1] "John Rob"
+
+name1<-paste0(c1,c2)
+name1
+# [1] "JohnRob"
+
+name<-paste(c1,c2,sep=",")
+name
+# [1] "John,Rob"
+
+newname<-sub("Rob","Cena",name) # Replaces set of characters
+newname
+# [1] "John,Cena"
+
+length(c1)
+# [1] 1
+
+nchar(c1)
+# [1] 4
+
+
+
+
+#### functions and flow control ####
+# Logical Operations
+# And - &
+# Or - |
+# Not - !
+
+x <- 25
+x < 10
+# [1] FALSE
+
+x > 10
+# [1] TRUE
+
+x < 20 & x > 10
+# [1] FALSE
+
+(x > 20) & (x>5) & (x == 25)
+# [1] TRUE
+
+x > 10 | x < 5
+# [1] TRUE
+
+x < 20 | x == 30
+# [1] FALSE
+
+(12 == 3)
+# [1] FALSE
+
+!(12 == 3)
+# [1] TRUE
+
+
+df <- mtcars
+df
+
+df[df['mpg'] >= 30,] # Notice the use of indexing with the comma
+#                 mpg cyl disp  hp drat    wt  qsec vs am gear carb
+# Fiat 128       32.4   4 78.7  66 4.08 2.200 19.47  1  1    4    1
+# Honda Civic    30.4   4 75.7  52 4.93 1.615 18.52  1  1    4    2
+# Toyota Corolla 33.9   4 71.1  65 4.22 1.835 19.90  1  1    4    1
+# Lotus Europa   30.4   4 95.1 113 3.77 1.513 16.90  1  1    5    2
+subset(df,mpg>=20)
+#                 mpg cyl  disp  hp drat    wt  qsec vs am gear carb
+# Mazda RX4      21.0   6 160.0 110 3.90 2.620 16.46  0  1    4    4
+# Mazda RX4 Wag  21.0   6 160.0 110 3.90 2.875 17.02  0  1    4    4
+# Datsun 710     22.8   4 108.0  93 3.85 2.320 18.61  1  1    4    1
+#Hornet 4 Drive 21.4   6 258.0 110 3.08 3.215 19.44  1  0    3    1
+# Merc 240D      24.4   4 146.7  62 3.69 3.190 20.00  1  0    4    2
+# Merc 230       22.8   4 140.8  95 3.92 3.150 22.90  1  0    4    2
+# Fiat 128       32.4   4  78.7  66 4.08 2.200 19.47  1  1    4    1
+# Honda Civic    30.4   4  75.7  52 4.93 1.615 18.52  1  1    4    2
+# Toyota Corolla 33.9   4  71.1  65 4.22 1.835 19.90  1  1    4    1
+# Toyota Corona  21.5   4 120.1  97 3.70 2.465 20.01  1  0    3    1
+# Fiat X1-9      27.3   4  79.0  66 4.08 1.935 18.90  1  1    4    1
+# Porsche 914-2  26.0   4 120.3  91 4.43 2.140 16.70  0  1    5    2
+# Lotus Europa   30.4   4  95.1 113 3.77 1.513 16.90  1  1    5    2
+# Volvo 142E     21.4   4 121.0 109 4.11 2.780 18.60  1  1    4    2
+
+df[(df['mpg'] >= 20) & (df['hp'] > 100),]
+
+
+
+# ----------Flow Control -----------------#
